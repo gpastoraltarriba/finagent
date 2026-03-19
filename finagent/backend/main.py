3ded7591c96154637990f6867bd6e09077608234
Gerard <g.pastor.altarriba@gmail.com>
2026-03-19 22:44:55 +0100

    Fix regex raw string

--- finagent/backend/main.py
+++ finagent/backend/main.py
@@ -17,7 +17,7 @@ yf.set_tz_cache_location("/tmp")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="https://.*\.vercel\.app",
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
